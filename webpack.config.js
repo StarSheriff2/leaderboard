2fee26fea066e7b499ec8e636b75016490541e6e
Alonso Falconi <alonso.fl@live.com>
2021-07-27 00:05:45 -0500

    Fix Lighthouse linter error. The page did not paint any content

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackLighthousePlugin = require('webpack-lighthouse-plugin');
 
 module.exports = {
   mode: 'development',
@@ -16,9 +15,6 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-    }),
-    new WebpackLighthousePlugin({
-      url: 'http://localhost:9001',
     }),
   ],
   module: {
