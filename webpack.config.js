490ed099d6e9230b80875e098dedff4bb116dafa
Alonso Falconi <alonso.fl@live.com>
2021-07-26 23:35:05 -0500

    Lighthouse linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackLighthousePlugin = require('webpack-lighthouse-plugin');
 
 module.exports = {
   mode: 'development',
@@ -15,6 +16,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+    }),
+    new WebpackLighthousePlugin({
+      url: 'http://localhost:36507'
     }),
   ],
   module: {
